badde00e8f0bc8ccfef667473d5b190c02ced518
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2024-09-17 01:55:02 +0200

    entite rdv + getter et setter

--- toubeelib/app/src/core/domain/entities/rdv/RendezVous.php
+++ toubeelib/app/src/core/domain/entities/rdv/RendezVous.php
@@ -1,38 +1,75 @@
 <?php
-
 namespace toubeelib\core\domain\entities\rdv;
 
 use toubeelib\core\domain\entities\Entity;
-use toubeelib\core\dto\RendezVousDTO;
+use toubeelib\core\domain\entities\praticien\Specialite;
 
-
-class RendezVous extends Entity{
+class RendezVous extends Entity
+{
     protected string $praticienID;
     protected string $patientID;
-    protected string $date;
-    protected bool $type;
-    protected bool $newPatient;
-    protected string $status;   // définir un statut
-    protected ?Specialite $specialite = null;
+    protected \DateTimeImmutable $date; 
+    protected bool $type; // true pour les téléconsultations, false pour le présentiel
+    protected bool $newPatient; // true si le patient est nouveau, false sinon
+    protected string $status; // OK si le patient a honoré le rendez-vous, KO si ce n'est pas le cas, EN ATTENTE si le rendez-vous n'a pas encore eu lieu
+    protected string $specialiteID;
 
-    public function __construct(string $praticien, string $patient, string $date, bool $type, bool $newPatient)
+    public function __construct(string $praticien, string $patient, string $specialiteID, \DateTimeImmutable $date) // bool $type, bool $newPatient)
     {
         $this->praticienID = $praticien;
         $this->patientID = $patient;
+        $this->specialiteID = $specialiteID;
         $this->date = $date;
-        $this->type = $type;
-        $this->newPatient = $newPatient;
+        // $this->type = $type; 
+        // $this->newPatient = $newPatient;
+        $this->status = 'EN ATTENTE';
     }
 
     public function setSpecialite(Specialite $specialite): void
     {
-        $this->specialite = $specialite;
+        $this->specialite = $specialite; 
     }
 
-    public function toDTO(): RendezVousDTO
+    public function setNewPatient(bool $newPatient): void
     {
-        return new RendezVousDTO($this);
+        $this->newPatient = $newPatient;
     }
 
+    public function setType(bool $type): void
+    {
+        $this->type = $type;
+    }
 
+    /*
+     * Getters et setters
+     */
+    public function getPraticienID(): string
+    {
+        return $this->praticienID;
+    }
+
+    public function getPatientID(): string
+    {
+        return $this->patientID;
+    }
+
+    public function getSpecialite(): string
+    {
+        return $this->specialiteID;
+    }
+
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
 }
