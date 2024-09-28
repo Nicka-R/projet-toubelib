5ceb3d60da0650490c3dc6119359abafcb32ee4d
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2024-09-28 11:38:21 +0200

    prendre en compte l'annulation d'un rdv

--- toubeelib/app/tests/rdv.service.test.php
+++ toubeelib/app/tests/rdv.service.test.php
@@ -100,6 +100,8 @@ try {
     );
 
     $rdv6 = $serviceRdv->creerRendezVous($inputRdv);
+    $serviceRdv->annulerRendezVous($rdv6->getId());     //il faut adapter le code pour ajouter une dispo quand un rdv est annulé
+                                                        // genre mettre à jour dans la méthode annulerRendezVous
 
     // lister les rendez-vous d'un praticien à une date donnée
     // $rdvs = $serviceRdv->listerRendezVousPraticien('p1', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-10-11 09:00'), \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-10-11 11:00'));
