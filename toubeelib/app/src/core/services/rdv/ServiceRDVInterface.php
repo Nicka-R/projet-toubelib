e9786f993824ab95079cce61a94cc1f9b33fa902
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2024-10-08 14:20:53 +0200

    Fix file names with uppercase letters

--- toubeelib/app/src/core/services/rdv/ServiceRDVInterface.php
+++ toubeelib/app/src/core/services/rdv/ServiceRDVInterface.php
@@ -1,17 +1,20 @@
 <?php
 
 namespace toubeelib\core\services\rdv;
-use toubeelib\core\dto\RDVDTO;
-use toubeelib\core\dto\InputRDVDTO;
 
+use toubeelib\core\dto\RendezVousDTO;
+use toubeelib\core\dto\InputRdvDTO;
+use toubeelib\core\dto\SpecialiteDTO;
 
-interface ServiceRDVInterface
+interface ServiceRdvInterface
 {   
     public function getRDVs() : array ;
-    public function getRDVByID(string $id) : RDVDTO ;
-    public function creerRendezvous(InputRDVDTO $r): RDVDTO ;
-    public function annulerRendervous(string $id): RDVDTO ;
-    public function modifierPatient(string $id, string $patient): RDVDTO ;
-    public function modifierSpecialite(string $id, string $specialite): RDVDTO ;
-
+    public function getRendezVousById(string $id): RendezVousDTO;
+    public function creerRendezVous(InputRdvDTO $inputRDV): RendezVousDTO;
+    public function getSpecialiteById(string $id): SpecialiteDTO;
+    public function isPraticienAvailable(string $praticien_id, \DateTimeImmutable $date): bool;
+    public function checkPraticienSpecialites(string $praticienId, string $specialite): bool;
+    public function listerDisposPraticien($praticien_id, \DateTimeImmutable $from, \DateTimeImmutable $to);
+    public function listerRendezVousPraticien(string $praticien_id, \DateTimeInterface $dateDebut, int $nbJours): array;
+    public function modifierRDV(string $rdvID, string $speID = null, string $patientID = null) :  RendezVousDTO;
 }
