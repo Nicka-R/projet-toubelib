ac9b59bcc65da28526754c4c6e1d299270de560a
odinalx <odin.alexandre26@gmail.com>
2024-10-08 17:41:42 +0200

    fix

--- toubeelib/app/sql/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO specialite (label, description)
-VALUES ('Dentiste', 'Spécialiste des dents');
-
-
-INSERT INTO praticien (nom, prenom, adresse, telephone, specialite_id)
-VALUES ('Dupont', 'Jean', '123 Rue de la Paix', '0123456789', '2c32af3a-f1c1-4e85-ab1d-890dcac332fb');
-
-INSERT INTO patientAdmin (nom, prenom, nss, date_naissance, adresse, mail, medecin_traitant)
-VALUES ('Georges', 'Victor', '123456789012345', '1990-01-01', '123 Rue de la Paix', 'victorgeorges54@gmail.com', 'Michel');
