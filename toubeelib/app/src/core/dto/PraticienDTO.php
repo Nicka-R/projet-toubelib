6e2043578707cc0225e42a7247d5be5d12b39b41
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2024-10-20 20:57:40 +0200

    update DTO praticien

--- toubeelib/app/src/core/dto/PraticienDTO.php
+++ toubeelib/app/src/core/dto/PraticienDTO.php
@@ -30,33 +30,4 @@ class PraticienDTO extends DTO
         return $p;
     }
 
-    public function getID(): string
-    {
-        return $this->ID;
-    }
-
-    public function getNom(): string
-    {
-        return $this->nom;
-    }
-
-    public function getPrenom(): string
-    {
-        return $this->prenom;
-    }
-
-    public function getAdresse(): string
-    {
-        return $this->adresse;
-    }
-
-    public function getTel(): string
-    {
-        return $this->tel;
-    }
-
-    public function getSpecialiteLabel(): string
-    {
-        return $this->specialite_label;
-    }
 }
