71750ec1e5275861801cd60da2100a1856b47572
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2024-10-20 20:58:25 +0200

    update des routes avec ajout des middlewares

--- toubeelib/app/config/routes.php
+++ toubeelib/app/config/routes.php
@@ -1,40 +1,32 @@
 <?php
-declare(strict_types=1);
 
+use Slim\App;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Slim\App;
-use \toubeelib\application\actions\ModifierRDVAction;
-use \toubeelib\application\actions\RDVbyIDAction;
-use \toubeelib\application\actions\HomeAction;
-use \toubeelib\application\actions\PraticienbyIDAction;
-use \toubeelib\application\actions\AuthAction;
-use \app\middlewares\Cors;
-use \app\middlewares\CheckJwtToken;	
-
-
+use app\middlewares\auth\CheckJwtToken;
+use app\middlewares\auth\AuthrzPraticienMiddleware;
+use toubeelib\core\services\auth\AuthrzService;
+use toubeelib\application\actions\HomeAction;
+use toubeelib\application\actions\RDVbyIDAction;
+use toubeelib\application\actions\ModifierRDVAction;
+use toubeelib\application\actions\PraticienbyIDAction;
+use toubeelib\application\actions\AuthAction;
+use app\middlewares\cors\Cors;
 
 return function(App $app): App {
     $app->add(Cors::class);
-    $app->add(CheckJwtToken::class);
 
+    // Public routes
     $app->get('/', HomeAction::class)->setName('home');
-
+    $app->post('/auth/signin', AuthAction::class)->setName('authSignin');
     $app->get('/rdvs/{id}', RDVbyIDAction::class)->setName('rdvById');
-
     $app->patch('/rdvs/{id}/modifier', ModifierRDVAction::class)->setName('modifierRDV');
 
-    $app->get('/praticiens/{id}', PraticienbyIDAction::class)->setName('praticienById');
-
-    $app->get('/auth/signin', AuthAction::class)->setName('authSignin');
-
-    $app->post('/auth/signin', AuthAction::class)->setName('authSignin');
+    $app->get('/praticiens/{id}', PraticienbyIDAction::class)->setName('praticienById')
+                                                            ->add(CheckJwtToken::class);
 
     $app->options('/{routes:.+}', function (Request $request, Response $response) {
         return $response;
-    })->add(function (Request $request, RequestHandler $handler) {
-        $cors = new Cors();
-        return $cors->corsHeaders($request, $handler);
     });
 
     return $app;
