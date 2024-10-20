eb0d7750a38d2feaa30e9e337fdf24e6eb955375
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2024-10-20 20:57:26 +0200

    ajout des constantes dasn user

--- toubeelib/app/src/core/domain/entities/user/User.php
+++ toubeelib/app/src/core/domain/entities/user/User.php
@@ -4,6 +4,15 @@ namespace toubeelib\core\domain\entities\user;
 use toubeelib\core\domain\entities\Entity;
 
 class User extends Entity{
+    const PATIENT = 0;
+    const ROLE_PERSONNEL_MEDICAL= 5;
+    const ROLE_PRACTICIEN = 10;
+    const ROLE_ADMIN = 15;
+
+    const PERMISSION_READ = 1;
+    const PERMISSION_WRITE = 2;
+    const PERMISSION_DELETE = 3;
+
     protected string $id;
     protected string $email;
     protected string $password;
